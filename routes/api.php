2509b263b09fbe48da2eff279665aa27166bcc8a
alvin jose <alvinjoseorcullo@gmail.com>
2021-03-26 21:38:13 +0800

    first commit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,12 +15,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/post');
+// Route::get('/posts', function() {
+//     $post = Post::create([
+//         'title'=>'my first post',
+//         'slug'=>'my-first-post'
+//     ]);
+
+//     return $post;
+// });
+
+// Route::post('/post')
+
+// Route::put('/posts/{id}')
+
+// Route::delete('/posts/{id}')
 
 // Route::get('/testing-the-api', function()  {
 //     return ['message' => 'hello'];
 // });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function() {
+    Route::apiResource('posts', 'PostController');
 });
+
+Route::get('/comments/{commentId}')
+
+
+// Route::get('/posts', 'PostController@index');
+// Route::post('/posts', 'PostController@store');
+// Route::put('/posts', 'PostController@update');
+// Route::delete('/posts', 'PostController@destroy');
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
